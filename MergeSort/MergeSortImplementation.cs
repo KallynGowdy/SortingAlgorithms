173979ab5ef825e2754d6727f2553a0d0fa536e5
unknown <gowdyk@ATC-176-11.ferris.local>
2015-02-03 16:23:19 -0500

    Added Merge Sort with Recursion

---
+++ MergeSort/MergeSortImplementation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    /// <summary>
+    /// Defines a static class that contains implementations of merge sort.
+    /// </summary>
+    public static class MergeSortImplementation
+    {
+
+        /// <summary>
+        /// Sorts the given numbers in ascending order using the merge sort algorithm.
+        /// This variant uses recursion to sort the items. It kinda "cheats" on splitting items because it recognizes that
+        /// at the end of the splitting process each item is in an array of 1, so just put each number into its own array.
+        /// Merging uses recursion though by taking two lists at a time and merging them in their sorted order. Then it takes the result of
+        /// those mergers and runs it again.
+        /// </summary>
+        /// <param name="numbers">The numbers that should be sorted.</param>
+        /// <returns>Returns a new array that represents the sorted numbers.</returns>
+        public static int[] SortUsingRecursion(int[] numbers)
+        {
+            return Merge(numbers.Select(n => new int[] { n }).ToArray());
+        }
+
+
+        private static int[] Merge(params int[][] lists)
+        {
+            if (lists.Length <= 1)
+            {
+                return lists.First();
+            }
+            else
+            {
+                List<int[]> results = new List<int[]>();
+                for (int i = 1; i < lists.Length; i += 2)
+                {
+                    results.Add(Merge(lists[i - 1], lists[i]));
+                }
+                return Merge(results.ToArray());
+            }
+        }
+
+        private static int[] Merge(int[] firstList, int[] secondList)
+        {
+            int[] result = new int[firstList.Length + secondList.Length];
+            int[] shortest = firstList.Length > secondList.Length ? firstList : secondList;
+            int[] longest = firstList.Length > secondList.Length ? secondList : firstList;
+            for (int i = 0; i < shortest.Length; i += 2)
+            {
+                if (shortest[i] < longest[i])
+                {
+                    result[i] = shortest[i];
+                    result[i + 1] = longest[i];
+                }
+                else
+                {
+                    result[i] = longest[i];
+                    result[i + 1] = shortest[i];
+                }
+            }
+            for (int i = shortest.Length; i < longest.Length; i++)
+            {
+                result[(shortest.Length * 2 + (i - shortest.Length))] = longest[i];
+            }
+            return result;
+        }
+
+    }
+}
---
+++ MergeSortTests/MergeSortTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MergeSort;
+
+namespace MergeSortTests
+{
+    [TestClass]
+    public class MergeSortTests
+    {
+        [TestMethod]
+        public void TestMergeSortWithRecursion()
+        {
+            TestHelpers.TestHelpers.TestAlgorithm(MergeSortImplementation.SortUsingRecursion, 10000);
+        }
+    }
+}
